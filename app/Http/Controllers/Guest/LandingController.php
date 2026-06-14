7ad5ca4f0815a14e65b16d4f6f13c97aa3959a0e
JasaSK <jasasatu703@gmail.com>
2026-06-14 20:51:44 +0700

    fix

--- app/Http/Controllers/Guest/LandingController.php
+++ app/Http/Controllers/Guest/LandingController.php
@@ -11,8 +11,13 @@ class LandingController extends Controller
 {
     public function index()
     {
-        $categories = Category::all();
-        $products = Product::with('images')->latest()->take(6)->get();
+        $categories = Category::orderBy('name')->get();
+
+        $products = Product::with(['images', 'category'])
+            ->latest()
+            ->take(6)
+            ->get();
+
         return view('guest.landing.index', compact('products', 'categories'));
     }
 }
--- app/Http/Controllers/Guest/ProductListController.php
+++ app/Http/Controllers/Guest/ProductListController.php
@@ -11,15 +11,20 @@ class ProductListController extends Controller
 {
     public function index()
     {
-        $products = Product::with('images')->get();
-        $categories = Category::all();
+        $products = Product::with(['images', 'category'])
+            ->latest()
+            ->get();
+
+        $categories = Category::orderBy('name')->get();
+
         return view('guest.product.index', compact('products', 'categories'));
     }
 
-
     public function detail($id)
     {
-        $product = Product::with('images')->findOrFail($id);
+        $product = Product::with(['images', 'category'])
+            ->findOrFail($id);
+
         return view('guest.product.detail', compact('product'));
     }
 }
